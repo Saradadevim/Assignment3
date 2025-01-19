18425ddec6ac5ac49c7106629f1a5e58097a1875
Saradadevim <saradakukkuu15@gmail.com>
2025-01-19 20:49:12 +0530

    Add files via upload

---
+++ Assignment3-3C.cs
@@ -0,0 +1,96 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Assignment3_3C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //separate odd and even integers in separate arrays.
+
+            //declaring a variable
+            int size;
+
+            //getting user input for array size
+            Console.Write("Enter array size: ");
+
+            //Validating
+            while(!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            {
+                Console.WriteLine("Invalid Inuput!! Enter a Positive number: ");
+            }
+
+            //declaring variables for array
+            int[] numbers = new int[size];
+            int i;
+
+            //loop to add the elements in the loop
+            for (i = 0; i < size; i++)
+            {
+                Console.Write($"Element{i + 1}: ");
+
+                //validating array elements
+                while (!int.TryParse(Console.ReadLine(), out numbers[i]))
+                {
+                    Console.WriteLine("Invalid Input!! Enter a positive number. ");
+                    Console.Write($"Element{i + 1}: ");
+                }
+            }
+
+            //declaring array for saving even and odd numbers separately
+            int[] oddNumbers = new int[numbers.Length];
+            int[] evenNumbers = new int[numbers.Length];
+
+            int evenCount = 0;  
+            int oddCount = 0;
+
+            //to check whether the numbers are even or not
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenNumbers[evenCount] = num;
+                    evenCount++;
+                }
+                else
+                {
+                    oddNumbers[oddCount] = num;
+                    oddCount++;
+                }
+            }
+
+            // Displaying Even Numbers
+            Console.WriteLine("\nEven Numbers in the array:");
+            if (evenCount == 0)
+            {
+                Console.WriteLine("No even numbers found.");
+            }
+            else
+            {
+                for (i = 0; i < evenCount; i++)
+                {
+                    Console.Write(evenNumbers[i] + " ");
+                }
+            }
+
+            // Displaying Odd Numbers
+            Console.WriteLine("\n\nOdd Numbers in the array:");
+            if (oddCount == 0)
+            {
+                Console.WriteLine("No odd numbers found.");
+            }
+            else
+            {
+                for (i = 0; i < oddCount; i++)
+                {
+                    Console.Write(oddNumbers[i] + " ");
+                }
+            }
+
+
+
+            Console.WriteLine("\nPress any key to Exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment3-4D.cs
@@ -0,0 +1,56 @@
+﻿namespace Assignment3_4D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // sort elements of the array in descending order.
+            //getting user input as array size
+            Console.Write("Enter the number of elements in the array: ");
+            int size;
+
+            //validating
+            while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            {
+                Console.WriteLine("Please enter a valid positive integer for the array size:");
+            }
+
+            // declaring the array
+            int[] array = new int[size];
+
+            // Getting array elements from the user with validation
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Enter  element  {i + 1}: ");
+                while (!int.TryParse(Console.ReadLine(), out array[i]))
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer value.");
+                    Console.Write($"Enter integer value for element {i + 1}: ");
+                }
+            }
+
+            // loop for sorting using indices
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        // Swaping the elements
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            // Displaying the sorted array
+            Console.WriteLine("Array elements in descending order:");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
